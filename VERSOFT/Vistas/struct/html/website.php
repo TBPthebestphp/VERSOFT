a6247f9d357b66a883773f3581f65d3a2b143aa5
ramiro <miobjetophp@gmail.com>
2023-11-10 01:11:26 -0700

    updating

--- VERSOFT/Vistas/struct/html/website.php
+++ VERSOFT/Vistas/struct/html/website.php
@@ -143,15 +143,24 @@ require_once("VERSOFT/Vistas/struct/piezas4websites/form/form.php");
                 </li>
                 <li><a href="http://blog.local">
                     versoft - blog
-                    
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-article" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
-                    <path d="M7 8h10"></path>
-                    <path d="M7 12h10"></path>
-                    <path d="M7 16h10"></path>
+                    <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path>
+                    <path d="M11 13l9 -9"></path>
+                    <path d="M15 4h5v5"></path>
                     </svg>
                 </a></li>
+                <li>
+                    <a href="http://adminer.local/adminer.php">
+                        Adminer
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path>
+                        <path d="M11 13l9 -9"></path>
+                        <path d="M15 4h5v5"></path>
+                        </svg>
+                    </a>
+                </li>
             </ul>
         </nav>
 
@@ -437,50 +446,6 @@ require_once("VERSOFT/Vistas/struct/piezas4websites/form/form.php");
 
         <footer>
             <p>Todos los Derechos reservados &copy;Ramiro G Gonzalez 2023.</p>
-            <div>
-                <form>
-                    <fieldset>
-                        <legend>
-                            Inicia Enviando me tus datos aqui Please !  
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-heart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <path d="M21 12a9 9 0 1 0 -8.012 8.946" />
-                            <path d="M9 10h.01" />
-                            <path d="M15 10h.01" />
-                            <path d="M9.5 15a3.59 3.59 0 0 0 2.774 .99" />
-                            <path d="M18.994 21.5l2.518 -2.58a1.74 1.74 0 0 0 .004 -2.413a1.627 1.627 0 0 0 -2.346 -.005l-.168 .172l-.168 -.172a1.627 1.627 0 0 0 -2.346 -.004a1.74 1.74 0 0 0 -.004 2.412l2.51 2.59z" />
-                            </svg>
-                        </legend>
-
-
-                        <label for="nombre">
-                            Nombre
-                            <input type="text" id="nombre" name="nombre"
-                            placeholder="Tu Nombre">
-                        </label>
-
-                        <label for="Telefono">
-                            Telefono
-                            <input type="tel" id="telefono" name="telefono"
-                            placeholder="Tu Telefono">
-                        </label>
-
-                        <label for="correo">
-                            Correo
-                            <input type="email" id="correo" name="correo"
-                            placeholder="Tu Email">
-                        </label>
-
-                        <label for="mensaje">
-                            Mensaje
-                            <textarea id="mensaje"></textarea>
-                        </label>
-
-                        <input type="submit" value="Enviar"> 
-
-                    </fieldset>
-                </form>
-            </div>
         </footer>
 
     </main>
